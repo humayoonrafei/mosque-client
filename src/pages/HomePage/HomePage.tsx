8596c3383db1df964f504a3394746145d395fa7e
Richard Prasad <richarddprasad@gmail.com>
2020-02-08 10:06:25 -0800

    Added Material UI support: Installed packages and updated tsconfig.json options

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import styled from 'styled-components';
+import Button from '@material-ui/core/Button';
 import { Dispatch } from 'redux';
 import { Redirect } from 'react-router-dom';
 import { connect, MapStateToProps, MapDispatchToProps } from 'react-redux';
@@ -11,23 +12,21 @@ import {
 } from './IHomePage';
 import { SERVER_URL } from '../../constants/constants';
 
-const HomeContainer = styled.div`
-    display: grid;
-    grid-template-rows: [logo-start] 22rem
-                        [logo-end form-start] 24rem 
-                        [form-end forgot-btn-start] 4rem
-                        [forgot-btn-end];
-    align-content: center;
-    justify-content: center;
-    align-items: center;
-    justify-items: center; 
+const TestButton = styled(Button)`
+  background-color: #6772e5;
+  color: #fff;
+  box-shadow: 0 4px 6px rgba(50, 50, 93, 0.11), 0 1px 3px rgba(0, 0, 0, 0.08);
+  padding: 7px 14px;
+  &:hover {
+    background-color: #5469d4;
+  }
 `;
 
 const HomePageComponent: React.FC<IHomePageOwnProps & IHomePageStateToProps & IHomePageDispatchProps> =
     ({ userState }): JSX.Element => {
         return (
             <div>
-                This is the home page.
+                <TestButton>Material UI Button</TestButton>
             </div>
         )
     };
